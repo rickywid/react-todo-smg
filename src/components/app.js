b458e896690fc8306e8f717b7e93dc93e67f5ab3
Ricky JW <rickywid@hotmail.com>
2017-02-05 16:38:48 -0500

    add some html to app.js file

--- src/components/app.js
+++ src/components/app.js
@@ -2,17 +2,21 @@ import React from 'react';
 
 const App = () => {
 	return (
-		<form action="" className="form">
+		<div>
+			<p>* Sass Test</p>
+			<p>* Flexbox Test</p>
+			<form action="" className="form">
 
-			<div className="form-row">
-				<label htmlFor="name">name</label>
-				<input id="name" type="text" />
-			</div>
-			<div className="form-row">
-				<label htmlFor="password">password</label>
-				<input id="password" type="text" />
-			</div>
-		</form>
+				<div className="form-row">
+					<label htmlFor="name">name</label>
+					<input id="name" type="text" />
+				</div>
+				<div className="form-row">
+					<label htmlFor="password">password</label>
+					<input id="password" type="text" />
+				</div>
+			</form>
+		</div>
 	);
 };
 
