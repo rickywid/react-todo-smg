a06eb1a2e67c0aa63284c4576cd3a3c86d0e26c5
Ricky JW <rickywid@hotmail.com>
2017-02-06 08:59:34 -0500

    include bootstrap-sass files in project

--- src/components/app.js
+++ src/components/app.js
@@ -3,19 +3,11 @@ import React from 'react';
 const App = () => {
 	return (
 		<div>
-			<p>* Sass Test</p>
-			<p>* Flexbox Test</p>
-			<form action="" className="form">
+			<div className="row">
+				<div className="left">column 1</div>
+				<div className="right">column 2</div>
+			</div>
 
-				<div className="form-row">
-					<label htmlFor="name">name</label>
-					<input id="name" type="text" />
-				</div>
-				<div className="form-row">
-					<label htmlFor="password">password</label>
-					<input id="password" type="text" />
-				</div>
-			</form>
 		</div>
 	);
 };
